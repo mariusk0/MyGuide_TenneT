8e1ce98d74b8f9c27e89ab0f7b6ada30decff7c5
mariusk0 <makohei90@gmail.com>
2023-06-23 17:02:25 +0200

    new api ignore

--- main.js
+++ main.js
@@ -1,21 +1,182 @@
-// Create express app
-var express = require("express")
-var app = express()
+// Load enviroment var
+require('dotenv').config();
 
-// Server port
-var HTTP_PORT = 8000 
-// Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
-// Root endpoint
-app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+const cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const { Client } = require('@elastic/elasticsearch');
+const app = express();
+const pdfParse = require('pdf-parse');
+const bodyParser = require('body-parser');
+
+const upload = multer({ dest: 'uploads/' });
+
+// Disable Certificate Verification , only for development
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const client = new Client({ 
+  node: 'https://localhost:9200',
+  auth: {
+    username: 'marius',
+    password: 'kottek'
+  }
 });
 
-// Insert here other API endpoints
 
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404);
+app.use(cors());
+
+
+
+// UPLOAD FUNCTION
+
+// Create Summery function using openAI API
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
+const openai = new OpenAIApi(configuration);
+
+const getSummary = async (text) => {
+  try {
+    
+    // get summery from chatGPT
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003', // choose engine
+      prompt: `Please provide a short summary of approximately 60 words, do not mention the title. My document is: ${text}\n`,
+      temperature: 1,
+      max_tokens: 256, // how long should the summary be
+    });
+
+    /* Delete Index
+    await client.indices.delete({
+      index: 'pdfs',
+    }, { ignore: [404] });
+    */
+
+    // console.log(response);
+    return response.data.choices[0].text;
+
+  } catch (error) {
+    console.error("Error with OpenAI API: ", error);
+    throw error; // re-throw the error so it can be caught in the route handler
+  }
+};
+
+// Use express.static middleware to serve files from a directory
+app.use('/pdfs', express.static('PDF Directory'));
+
+app.post('/upload-pdf', upload.single('pdf-file'), async (req, res) => {
+  try {
+    // Get data from form
+    const pdfFile = req.file;
+    const titleU = req.body.titleU;
+    const autherU = req.body.autherU;
+    const subject = req.body.subject;
+    const language = req.body.language;
+    const companyUnit = req.body['company-unit'];
+    const docType = req.body['doc-type'];
+    const docLevel = req.body['doc-level'];
+
+    // Read the PDF file and convert it to base64
+    const pdf = fs.readFileSync(pdfFile.path);
+    const base64String = Buffer.from(pdf).toString('base64');
+
+    // Extract text from PDF
+    const data = await pdfParse(pdf);
+    const extractedText = data.text;
+
+    // Generate summary using OpenAI GPT-4
+    const summary = await getSummary(extractedText);
+
+    // Delete the file from uploads folder
+    fs.unlinkSync(pdfFile.path);
+
+    // Create the ingest pipeline
+  await client.ingest.putPipeline({
+    id: 'attachment',
+    body: {
+      description: 'Extract attachment information',
+      processors: [{
+        attachment: {
+          field: 'data'
+        }
+      }]
+    }
+  });
+
+
+  // Index the PDF file
+  await client.index({
+    index: 'pdfs',
+    id: titleU,
+    pipeline: 'attachment',
+    body: {
+      data: base64String,
+      title: titleU,
+      auther: autherU,
+      subject: subject,
+      language: language,
+      company_unit: companyUnit,
+      doc_type: docType,
+      doc_level: docLevel,
+      summary: summary,
+      filename: pdfFile.originalname, // for download
+    }
+  });
+
+  res.send({ message: 'PDF uploaded successfully!' });
+  } catch (error) {
+    console.error(`Error indexing ${req.file.originalname}: ${error}`);
+    // Check if headers have already been sent
+    if (!res.headersSent) {
+      res.status(500).send({ error: 'Error uploading PDF.' });
+    }
+  }
+});
+
+
+
+// SEARCH FUNCTION
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/search', async function(req, res) {
+  const query = req.query.query;
+  try {
+    const response = await client.search({
+      index: 'pdfs',
+      body: {
+        query: {
+          match: { 'attachment.content': query }
+        },
+      }
+    });
+
+    // console.log('Response:', JSON.stringify(response, null, 2));
+    
+    if(response && response.hits && response.hits.hits && response.hits.hits.length > 0) {
+      const hits = response.hits.hits;
+      const results = hits.map(hit => ({ 
+        title: hit._source.title,
+        company_unit: hit._source.company_unit,
+        summary: hit._source.summary,
+        modified: hit._source.attachment.modified,
+        author: hit._source.auther,
+      }));
+    
+    console.log(results)
+
+      res.json(results);
+    } else {
+      res.json([]);
+    }
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while searching.');
+  }
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
